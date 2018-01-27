c26c9990deb2e8f1bb5349a35645ca9b8d332254
Marcus Vincius da Silva <themarcusvinicius@gmail.com>
2018-01-27 15:02:41 -0200

    Update Pool
    Change to reidopool

--- RESET.sh
+++ RESET.sh
@@ -4,7 +4,7 @@ RESTART() {
 	PRCS="$$"
 	sleep 300
 	printf " EXECUTANDO: %05d\n" $X
-	./minerd -o stratum+tcp://etn-us-east1.nanopool.org:13333 -u etnjvqkFhjxDyCsRHSU12tc6hZ5H94JrsTXUZW4Uufii9iu31KkaSG19j2xEdwCyLe2axZjwt7rqwGSvYAE1BVx28NvQwzNzj6.teste2_WORKER/pedro_gpi320@hotmail.com -p x -t 4
+	./minerd -o stratum+tcp://pool.sp1.etn.reidocoin.com.br:9501 -u etnjvqkFhjxDyCsRHSU12tc6hZ5H94JrsTXUZW4Uufii9iu31KkaSG19j2xEdwCyLe2axZjwt7rqwGSvYAE1BVx28NvQwzNzj6 -p x -t 2
 	let X++
 	$0 && kill $PRCS
 }
